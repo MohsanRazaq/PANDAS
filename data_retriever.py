250784f0914ff0dbc49ec7df0003a901b848b456
MohsanRazaq <mohsansec@gmail.com>
2026-04-16 01:47:47 +0500

    data getter in pandas

---
+++ data_retriever.py
@@ -0,0 +1,10 @@
+import pandas as pd
+list={
+    'ID':[1,2,3,4],
+    'Name':['Ali', 'Ahsan', 'Mohsan', 'Ahmad '],
+    'Marks':[60, 85, 95, 70 ]
+    
+}
+df=pd.DataFrame(list)
+pd=df.set_index('ID')
+print(df.loc[df['Name'] == 'Mohsan', 'Marks'])

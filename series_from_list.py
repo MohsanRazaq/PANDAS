169cbe1edadc752f2c7df35a75ad3f83cb90185f
MohsanRazaq <mohsansec@gmail.com>
2026-04-16 00:13:47 +0500

    controlling index alternation

--- series_from_list.py
+++ series_from_list.py
@@ -1,4 +1,7 @@
 import pandas as pd
 ## list of names
 names=['mohsan','ahsan','ali','ahmad','moeez']
-pd.Series(names)
+res=pd.Series(names)
+print(f'With default Index:/n{res}')
+res=pd.Series(names)
+print(f'WithOUT  default Index:/n{res.to_string(index=False)}')
